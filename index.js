bfb2f30dc041610cfe9543281fd7f13bc342a445
Aman Sahu <amansahuxy@gmail.com>
2026-06-30 13:26:54 +0530

    Implement GST calculation logic

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const input=document.getElementById("inpNum");
-const input=document.getElementById("inpNum");
-const input=document.getElementById("inpNum");
-const input=document.getElementById("inpNum");
-const input=document.getElementById("inpNum");
+const gstRate=document.getElementById("gstRate");
+const Calculate=document.getElementById("calculate");
+const GST=document.getElementById("gst");
+const Final=document.getElementById("final");
+
+
+Calculate.addEventListener("click",()=>{
+    const amount=Number(input.value);
+const rate=Number(gstRate.value);
+const gstAmount=(amount*rate)/100;
+const finalAmount=amount+gstAmount;
+GST.innerText=`GST Amount: ₹${gstAmount}`;
+Final.innerText=`Total Amount: ₹${finalAmount}`;
+
+})
